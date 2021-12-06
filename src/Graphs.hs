05bd19867502e42d34e6414e7b49fccb33bc8fd9
groot <gauthamv93@yahoo.com>
2021-12-06 11:50:40 +0530

    Removing comments and renaming

--- src/Graphs.hs
+++ src/Graphs.hs
@@ -36,28 +36,16 @@ findGraph u (Node e1 n e2)
           nonIdGrs = filter ((/=n). getNode) graphs
           graphs  = map (\e -> dest e) (e1 ++ e2)
 
-idEdgeF :: Graph Unit -> Edge Unit
-idEdgeF idGr = edge idGr unitConv
-
-idEdgeR :: Graph Unit -> Edge Unit
-idEdgeR idGr = edge idGr (inverse unitConv)
-
-idEdge :: Graph Unit -> Edge Unit
-idEdge idGr = edge idGr unitConv
-
 edge :: Graph Unit -> Conversion -> Edge Unit
 edge gr s = Edge {dest=gr, conv=s}
 
--- Identity graph with self reference
-idGraph :: Unit -> Graph Unit
-idGraph u = newGraph (u, u, unitConv)
-
 mkIdGraph :: Unit -> Graph Unit
 mkIdGraph u = 
-    Node [fromEdg] u [toEdg]
-    where idGr    = mkIdGraph u
-          fromEdg = idEdgeR idGr
-          toEdg   = idEdgeF idGr
+    Node [inc] u [outg]
+    where idg  = mkIdGraph u
+          outg = edge idg unitConv
+          inc  = edge idg invUnitConv
+          invUnitConv = inverse unitConv
 
 connectGraphs 
     :: Graph Unit
@@ -65,57 +53,15 @@ connectGraphs
     -> Conversion 
     -> Graph Unit
 connectGraphs
-    gr1@(Node fs1 u1 ts1)
-    gr2@(Node fs2 u2 ts2)
-    conv = 
-        gr1'
+    gr1@(Node is1 u1 os1)
+    gr2@(Node is2 u2 os2)
+    conv = gr1'
     where
     gr2' = connectGraphs gr2 gr1 iconv
     iconv = inverse conv
-    t1'  = edge gr2' conv
-    f1'  = edge gr2' (inverse conv)
-    gr1' = Node (f1':fs1) u1 (t1':ts1)
-
--- connectGraphs :: Graph Unit -> Conversion -> Graph Unit -> Graph Unit
--- connectGraphs gr1 conv gr2 = 
---     ( Node (fromEdg:f1) u1 (toEdg:t1) )
---     where (Node f1 u1 t1) = gr1
---           (Node f2 u2 t2) = gr2
---           iconv = inverse conv
---           fromGr = connectGraphs gr2 iconv gr1
---           fromEdg = edge fromGr conv
---           toEdg = edge gr2 conv
-
--- connectGraphs :: Graph Unit -> Conversion -> Graph Unit -> Graph Unit
--- connectGraphs gr1@(Node f1 u1 t1) conv gr2@(Node f2 u2 t2) = 
---         (Node (f1 ++ [e2] u1 (t1 ++ [e1]))
-
---     where e1 = edge gr2 conv
---           e2 = edge (connectGraphs gr2 (inverse conv) gr1) conv
-
--- newGraph :: Relation -> Graph Unit
--- newGraph (from, to, conv) =
---     Node [idEg] from [toEg]
---     where toEg  = edge toGr iconv
---           toGr = newGraph (to, from, iconv)
---           iconv = inverse conv
-
---           idEg  = idEdge idGr
---           idGr = idGraph from
-
--- addNode :: Graph Unit -> Unit -> Conversion -> Graph Unit
--- addNode prevGr u conv = Node [prevEdge] u [idEg]
---     where idGr     = idGraph u
---           idEg     = idEdge idGr
---           prevEdge = edge prevGr (inverse conv)
-
--- mkGraph :: [Relation] -> Graph Unit-> Graph Unit
--- mkGraph []     _  = Empty
--- mkGraph (e:es) gr = case findGraph from gr of
---                          Nothing  -> newGraph e
---                          Just gr' -> let gr'' = addNode gr' to conv
---                                      in mkGraph es gr''
---                     where (from, to, conv) = e
+    o'  = edge gr2' conv
+    i'  = edge gr2' (inverse conv)
+    gr1' = Node (i':is1) u1 (o':os1)
 
 newGraph :: Relation -> Graph Unit
 newGraph (f, t, conv) = connectGraphs idf idt conv
@@ -137,27 +83,28 @@ mkGraph r@(f, t, conv) gr =
     where iconv = inverse conv
 
 moveF :: Graph Unit -> Graph Unit
-moveF (Node _ u (n:ns)) = dest n
+moveF (Node _ u (o:os)) = dest o
 
 moveR :: Graph Unit -> Graph Unit
-moveR (Node (n:ns) u _) = dest n
+moveR (Node (i:is) u _) = dest i
 
 getNode :: Graph Unit -> Unit
 getNode (Node _ n _) = n
 
-getEdgesF :: Graph Unit -> [Unit]
-getEdgesF (Node fs n ts) = map (getNode . dest) ts
+outgEdges :: Graph Unit -> [Unit]
+outgEdges (Node _ n os) = map (getNode . dest) os
 
-getEdgesR :: Graph Unit -> [Unit]
-getEdgesR (Node fs n ts) = map (getNode . dest) fs
+incEdges :: Graph Unit -> [Unit]
+incEdges (Node is n _) = map (getNode . dest) is
 
-getNonIdEdgeF :: Graph Unit -> [Unit]
-getNonIdEdgeF gr@(Node _ n ts) = filter ((/= n)) $ getEdgesF gr
+nonIdOutgEdges :: Graph Unit -> [Unit]
+nonIdOutgEdges gr@(Node _ n _) = filter (/= n) $ outgEdges gr
 
-getNonIdEdgeR :: Graph Unit -> [Unit]
-getNonIdEdgeR gr@(Node fs n _) = filter ((/= n)) $ getEdgesR gr
+nonIdIncEdges :: Graph Unit -> [Unit]
+nonIdIncEdges gr@(Node _ n _) = filter (/= n) $ incEdges gr
 
 pPrintGraph :: Graph Unit -> String
-pPrintGraph gr@(Node fs n ts) = "Node " ++ fs' ++ " " ++ n ++ " " ++ ts'
-                        where fs' = show $ getNonIdEdgeF gr
-                              ts' = show $ getNonIdEdgeR gr
+pPrintGraph gr@(Node is n os) = 
+    "Node " ++ is' ++ " " ++ n ++ " " ++ os'
+    where is' = show $ nonIdIncEdges gr
+          os' = show $ nonIdOutgEdges gr
